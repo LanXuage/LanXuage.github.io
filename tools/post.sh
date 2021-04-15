915aab0831186506438c1f4a4e373d1e6c7f0ae9
magicbook <1456817554@qq.com>
2021-04-15 20:26:09 +0800

    update

--- tools/post.sh
+++ tools/post.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-temp="---\ntitle: nasm学习汇编语言\nauthor: LanXuage\ndate: 2021-03-19 00:00:00 +0800\ncategories: [汇编]\ntags: [nasm, 汇编, gcc]\n---
-"
-echo $temp
+post_name=$1
+author="LanXuage"
+temp="---\ntitle: $post_name\nauthor: $author\ndate: `date '+%Y-%m-%d %T'` +0800\ncategories: []\ntags: []\n---"
+p=$2
+if [ ! -n "$p" ]; then
+    p=`pwd`
+fi
+e="`date '+%Y-%m-%d-'`$post_name"
+echo $temp > "$p/`echo $e|sed 's/ /_/g'`.md"
